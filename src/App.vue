4f86ef8c799e1265a4c480b77a46f40518eaccfd
imall <h1273@evertrust.com.tw>
2025-11-17 16:57:04 +0800

    initial-commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/vue.svg" />
+    <HelloWorld msg="歡迎使用 Vue 3 + Vite" />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">計數器：{{ count }}</button>
+    <p>
+      編輯
+      <code>src/components/HelloWorld.vue</code> 來測試 HMR
+    </p>
+  </div>
+
+  <p>
+    查看
+    <a href="https://vuejs.org/" target="_blank">Vue 文件</a>
+    和
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite 文件</a>
+    以了解更多資訊。
+  </p>
+  <p class="read-the-docs">點擊 Vue 和 Vite 標誌了解更多</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
