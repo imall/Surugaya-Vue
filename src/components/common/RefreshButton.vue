6e0efec307e033cd5f45b48ee5b514ee3273f788
imall <h1273@evertrust.com.tw>
2025-12-04 14:32:27 +0800

    style: 調整清除快取的 css

--- src/components/common/RefreshButton.vue
+++ src/components/common/RefreshButton.vue
@@ -2,7 +2,7 @@
   <BaseButton
     class="w-9 h-9 p-0! text-xl transition-all duration-200 active:translate-x-0.5 active:translate-y-0.5 active:shadow-sm disabled:opacity-50 shrink-0"
     variant="default" :disabled="loading" :title="loading ? '読み込み中...' : 'データを再読み込み'" @click="handleClick">
-    <span class="inline-block transition-transform duration-300" :class="{ 'animate-spin': loading }">↻</span>
+    <span class="flex items-center justify-center " :class="{ 'animate-spin': loading }">↻</span>
   </BaseButton>
 </template>
 
